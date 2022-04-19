e8bb6feecb73ae490efdc1cdb7f746d1dee6845b
杨志杰 <742452344@qq.com>
2022-04-19 18:07:36 +0800

    修改代码，新增dev

--- dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
+++ dubbodemo_consumer/src/main/java/com/atguigu/controller/ABC.java
@@ -3,6 +3,6 @@ package com.atguigu.controller;
 public class ABC {
 
     public static void main(String[] args) {
-        System.out.println("211111111111111111");
+        System.out.println("211111111111111111dev");
     }
 }
